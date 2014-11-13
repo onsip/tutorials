0465e1a654adf61c92671a01d2299331335d3244
Will Mitchell <will@onsip.com>
2014-11-13 18:51:26 -0500

    8 - Add destination input

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ function MyApp() {
   this.remoteMedia = document.getElementById('remote-media');
   this.remoteMedia.volume = 0.5;
 
+  this.destinationInput = document.getElementById('destination-input');
   this.inviteButton = document.getElementById('invite-button');
   this.inviteButton.addEventListener('click', this.sendInvite.bind(this), false);
 
@@ -29,10 +30,13 @@ MyApp.prototype = {
   },
 
   sendInvite: function () {
-    var session = this.ua.invite('welcome@onsip.com', this.remoteMedia);
+    var destination = this.destinationInput.value;
+    if (!destination) { return; }
+
+    var session = this.ua.invite(destination, this.remoteMedia);
 
     this.setSession(session);
-    this.inviteButton.disabled = true;
+    this.inviteButton.disabled = true; // TODO - use setStatus. Disable input, too?
   },
 
   setSession: function (session) {
