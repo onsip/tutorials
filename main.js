0c7859a2917f8c355cf6a3b117662fdad0752c24
Will Mitchell <will@onsip.com>
2014-11-25 20:20:49 -0500

    7 - Audio control with Volume and Mute

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 function MyApp() {
   this.userAgentDiv = document.getElementById('user-agent');
   this.remoteMedia = document.getElementById('remote-media');
+  this.remoteMedia.volume = 0.5;
 
   this.inviteButton = document.getElementById('invite-button');
   this.inviteButton.addEventListener('click', this.sendInvite.bind(this), false);
@@ -12,6 +13,15 @@ function MyApp() {
   document.addEventListener('keydown', function (e) {
     this.sendDTMF(String.fromCharCode(e.keyCode));
   }.bind(this), false);
+
+  this.volumeUp = document.getElementById('volume-up');
+  this.volumeUp.addEventListener('click', this.raiseVolume.bind(this), false);
+
+  this.volumeDown = document.getElementById('volume-down');
+  this.volumeDown.addEventListener('click', this.lowerVolume.bind(this), false);
+
+  this.muteButton = document.getElementById('mute-button');
+  this.muteButton.addEventListener('click', this.toggleMute.bind(this), false);
 }
 
 /* This is the MyApp prototype. */
@@ -69,6 +79,42 @@ MyApp.prototype = {
   sendDTMF: function (tone) {
     if (this.session) {
       this.session.dtmf(tone);
+    }
+  },
+
+  raiseVolume: function () {
+    this.volumeDown.disabled = false;
+
+    /* If volume is very high, jump to max to avoid rounding errors. */
+    if (this.remoteMedia.volume >= .85) {
+      this.remoteMedia.volume = 1;
+      this.volumeUp.disabled = true;
+    } else {
+      this.remoteMedia.volume += .1;
+    }
+  },
+
+  lowerVolume: function () {
+    this.volumeUp.disabled = false;
+
+    /* If volume is very low, jump to min to avoid rounding errors. */
+    if (this.remoteMedia.volume <= .15) {
+      this.remoteMedia.volume = 0;
+      this.volumeDown.disabled = true;
+    } else {
+      this.remoteMedia.volume -= .1;
+    }
+  },
+
+  toggleMute: function () {
+    if (!this.session) { return; }
+
+    if (this.muteButton.classList.contains('on')) {
+      this.session.unmute();
+      this.muteButton.classList.remove('on');
+    } else {
+      this.session.mute();
+      this.muteButton.classList.add('on');
     }
   },
 
