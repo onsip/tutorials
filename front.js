45a76426f061da2d01ca3eb0be843eb026f1abfe
Will Mitchell <will@onsip.com>
2014-12-29 12:51:51 -0500

    Auto-login

---
+++ front.js
@@ -0,0 +1,16 @@
+var demoToken;
+
+/* Get the demoToken */
+if (!window.localStorage) { return; }
+
+try {
+  demoToken = localStorage.getItem('demoToken');
+} catch(e) {}
+
+if (!demoToken || !(/^[a-zA-Z0-9]{6}$/).test(demoToken)) { return; }
+
+/* "Log in" */
+MyApp.createUA({
+  uri: 'front-b-instaphone.' + demoToken + '@disuo.onsip.com',
+  traceSip: true
+});
