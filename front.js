3398d75667e14c312059ead7bcf91bd1846e0acd
Will Mitchell <will@onsip.com>
2014-12-29 12:58:27 -0500

    Returns need to be in a function

--- front.js
+++ front.js
@@ -1,16 +1,18 @@
-var demoToken;
+(function () {
+  var demoToken;
 
-/* Get the demoToken */
-if (!window.localStorage) { return; }
+  /* Get the demoToken */
+  if (!window.localStorage) { return; }
 
-try {
-  demoToken = localStorage.getItem('demoToken');
-} catch(e) {}
+  try {
+    demoToken = localStorage.getItem('demoToken');
+  } catch(e) {}
 
-if (!demoToken || !(/^[a-zA-Z0-9]{6}$/).test(demoToken)) { return; }
+  if (!demoToken || !(/^[a-zA-Z0-9]{6}$/).test(demoToken)) { return; }
 
-/* "Log in" */
-MyApp.createUA({
-  uri: 'front-b-instaphone.' + demoToken + '@disuo.onsip.com',
-  traceSip: true
-});
+  /* "Log in" */
+  MyApp.createUA({
+    uri: 'front-b-instaphone.' + demoToken + '@disuo.onsip.com',
+    traceSip: true
+  });
+}());
