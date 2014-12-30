8f93f3827a49103348964adff56ce88b384725f8
Will Mitchell <will@onsip.com>
2014-12-30 09:42:44 -0500

    Enable video, Override Invite properly

--- front.js
+++ front.js
@@ -10,10 +10,15 @@
 
   if (!demoToken || !(/^[a-zA-Z0-9]{6}$/).test(demoToken)) { return; }
 
-  MyApp.sendInvite = function () {
+  /* Replace the Invite button with our special invite button */
+  var el = document.getElementById('invite-button');
+  var new_el = el.cloneNode(true);
+  el.parentNode.replaceChild(new_el,el);
+
+  new_el.addEventListener('click', function () {
     var session = this.ua.invite('front-b-instaphone.' + demoToken + '@disuo.onsip.com', this.remoteMedia);
 
     this.setSession(session);
     this.inviteButton.disabled = true;
-  }
+  }.bind(MyApp), true);
 }());
